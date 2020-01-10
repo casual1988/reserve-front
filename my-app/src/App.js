5c298bf1433eb6dc5f0a18394b7d339b815fc963
casual1988 <ale.ks.1988@hotmail.com>
2020-01-10 23:34:09 +0100

    login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import ListUserComponent from "./component/user/ListUserComponent";
-import AddUserComponent from "./component/user/AddUserComponent";
-import EditUserComponent from "./component/user/EditUserComponent";
+import AppRouter from "./component/RouterComponent";
 
 function App() {
-  return (
-      <div className="container">
-          <Router>
-              <div className="col-md-6">
-                  <h1 className="text-center" style={style}>React User Application</h1>
-                  <Switch>
-                      <Route path="/" exact component={ListUserComponent} />
-                      <Route path="/users" component={ListUserComponent} />
-                      <Route path="/add-user" component={AddUserComponent} />
-                      <Route path="/edit-user" component={EditUserComponent} />
-                  </Switch>
-              </div>
-          </Router>
-      </div>
-  );
-}
+    return (
 
-const style = {
-    color: 'red',
-    margin: '10px'
+        <AppRouter/>
+    );
 }
 
 export default App;
