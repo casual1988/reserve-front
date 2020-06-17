7a6eba4394194c9c165459c1279886768d96421b
lazovic89 <lazovic.dexter@gmail.com>
2020-06-17 22:50:40 +0200

    added state filed on user

--- my-app/src/component/user/AddUserComponent.jsx
+++ my-app/src/component/user/AddUserComponent.jsx
@@ -1,85 +1,166 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from "react";
 import UserService from "../../service/UserService";
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import NavBar from "../Navbar";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
 
-class AddUserComponent extends Component{
+class AddUserComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      employeeId: "",
+      contactNumber: "",
+      message: null,
+      state: 1,
+    };
+    this.saveUser = this.saveUser.bind(this);
+  }
 
-    constructor(props){
-        super(props);
-        this.state ={
-            username: '',
-            password: '',
-            firstName: '',
-            lastName: '',
-            employeeId: '',
-            contactNumber: '',
-            message: null
-        }
-        this.saveUser = this.saveUser.bind(this);
-    }
+  saveUser = (e) => {
+    e.preventDefault();
+    let user = {
+      username: this.state.username,
+      password: this.state.password,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      employeeId: this.state.employeeId,
+      contactNumber: this.state.contactNumber,
+      state: this.state.state,
+    };
+    UserService.addUser(user).then((res) => {
+      this.setState({ message: "User added successfully." });
+      this.props.history.push("/list-user");
+    });
+  };
 
-    saveUser = (e) => {
-        e.preventDefault();
-        let user = {username: this.state.username, password: this.state.password, firstName: this.state.firstName, lastName: this.state.lastName, employeeId: this.state.employeeId, contactNumber: this.state.contactNumber};
-        UserService.addUser(user)
-            .then(res => {
-                this.setState({message : 'User added successfully.'});
-                this.props.history.push('/list-user');
-            });
-    }
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
-    onChange = (e) =>
-        this.setState({ [e.target.name]: e.target.value });
+  render() {
+    return (
+      <Fragment>
+        <NavBar />
+        <Container>
+          <Typography variant="h4" style={styles.center}>
+            Dodaj Korisnika
+          </Typography>
+          <form style={formContainer}>
+            <TextField
+              label="USERNAME"
+              fullWidth
+              margin="normal"
+              name="username"
+              value={this.state.username}
+              onChange={this.onChange}
+            />
 
-    render() {
-        return(
-            <Fragment>
-                <NavBar/>
-                <Container>
-                    <Typography variant="h4" style={styles.center}>Dodaj Korisnika</Typography>
-                    <form style={formContainer}>
+            <TextField
+              label="PASSWORD"
+              type="password"
+              fullWidth
+              margin="normal"
+              name="password"
+              value={this.state.password}
+              onChange={this.onChange}
+            />
 
-                        <TextField label="USERNAME" fullWidth margin="normal" name="username" value={this.state.username} onChange={this.onChange}/>
+            <TextField
+              label="IME"
+              fullWidth
+              margin="normal"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.onChange}
+            />
 
-                        <TextField label="PASSWORD" type="password" fullWidth margin="normal" name="password" value={this.state.password} onChange={this.onChange}/>
+            <TextField
+              label="PREZIME"
+              fullWidth
+              margin="normal"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.onChange}
+            />
 
-                        <TextField label="IME" fullWidth margin="normal" name="firstName" value={this.state.firstName} onChange={this.onChange}/>
+            <TextField
+              label="ZASTUPNIK ID"
+              fullWidth
+              margin="normal"
+              name="employeeId"
+              value={this.state.employeeId}
+              onChange={this.onChange}
+            />
 
-                        <TextField label="PREZIME" fullWidth margin="normal" name="lastName" value={this.state.lastName} onChange={this.onChange}/>
+            <TextField
+              label="BROJ TELEFONA"
+              fullWidth
+              margin="normal"
+              name="contactNumber"
+              value={this.state.contactNumber}
+              onChange={this.onChange}
+            />
+            <FormControl fullWidth style={styles.state}>
+              <InputLabel id="demo-simple-select-label">Entitet</InputLabel>
+              <Select
+                name="state"
+                value={this.state.state}
+                onChange={this.onChange}
+              >
+                <MenuItem value={1}>Republika Srpska</MenuItem>
+                <MenuItem value={2}>Federacija BiH</MenuItem>
+              </Select>
+            </FormControl>
 
-                        <TextField label="ZASTUPNIK ID" fullWidth margin="normal" name="employeeId" value={this.state.employeeId} onChange={this.onChange}/>
-
-                        <TextField label="BROJ TELEFONA" fullWidth margin="normal" name="contactNumber" value={this.state.contactNumber} onChange={this.onChange}/>
-
-                        <Button variant="contained" style={styles.button} onClick={this.saveUser}>Snimi</Button>
-                        <Button variant="contained" style={styles.button} onClick={() => this.props.history.push('/list-user')}>Odustani</Button>
-                    </form>
-                </Container>
-            </Fragment>
-        );
-    }
+            <Button
+              variant="contained"
+              style={styles.button}
+              onClick={this.saveUser}
+            >
+              Snimi
+            </Button>
+            <Button
+              variant="contained"
+              style={styles.button}
+              onClick={() => this.props.history.push("/list-user")}
+            >
+              Odustani
+            </Button>
+          </form>
+        </Container>
+      </Fragment>
+    );
+  }
 }
 const formContainer = {
-    display: 'flex',
-    flexFlow: 'row wrap'
+  display: "flex",
+  flexFlow: "row wrap",
 };
 
-const styles= {
-    center :{
-        display: 'flex',
-        justifyContent: 'center',
-        marginTop: 10
-
-    },
-    button:{
-        background: '#8f2086',
-        color: "white",
-        margin:5
-    }
-}
+const styles = {
+  center: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  button: {
+    background: "#8f2086",
+    color: "white",
+    margin: 5,
+    marginBottom: 20,
+  },
+  state: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+};
 
 export default AddUserComponent;
--- my-app/src/component/user/EditUserComponent.jsx
+++ my-app/src/component/user/EditUserComponent.jsx
@@ -1,96 +1,180 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import UserService from "../../service/UserService";
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import NavBar from "../Navbar";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
 
 class EditUserComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      username: "",
+      firstName: "",
+      lastName: "",
+      employeeId: "",
+      contactNumber: "",
+      password: "",
+      state: 0,
+    };
+    this.saveUser = this.saveUser.bind(this);
+    this.loadUser = this.loadUser.bind(this);
+  }
 
-    constructor(props){
-        super(props);
-        this.state ={
-            id: '',
-            username: '',
-            firstName: '',
-            lastName: '',
-            employeeId: '',
-            contactNumber: '',
-            password: ''
-        }
-        this.saveUser = this.saveUser.bind(this);
-        this.loadUser = this.loadUser.bind(this);
-    }
+  componentDidMount() {
+    this.loadUser();
+  }
 
-    componentDidMount() {
-        this.loadUser();
-    }
+  loadUser() {
+    UserService.fetchUserById(window.localStorage.getItem("userId")).then(
+      (res) => {
+        let user = res.data.result;
+        this.setState({
+          id: user.id,
+          username: user.username,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          employeeId: user.employeeId,
+          contactNumber: user.contactNumber,
+          password: user.password,
+          state: user.state,
+        });
+      }
+    );
+  }
 
-    loadUser() {
-        UserService.fetchUserById(window.localStorage.getItem("userId"))
-            .then((res) => {
-                let user = res.data.result;
-                this.setState({
-                id: user.id,
-                username: user.username,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                employeeId: user.employeeId,
-                contactNumber: user.contactNumber,
-                password: user.password,
-                })
-            });
-    }
+  onChange = (e) => {
+    console.log("setting " + [e.target.name] + " to " + e.target.value);
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    onChange = (e) =>
-        this.setState({ [e.target.name]: e.target.value });
+  saveUser = (e) => {
+    e.preventDefault();
+    let user = {
+      id: this.state.id,
+      password: this.state.password,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      employeeId: this.state.employeeId,
+      contactNumber: this.state.contactNumber,
+      state: this.state.state,
+    };
+    UserService.editUser(user).then((res) => {
+      this.setState({ message: "User added successfully." });
+      this.props.history.push("/list-user");
+    });
+  };
 
-    saveUser = (e) => {
-        e.preventDefault();
-        let user = {id: this.state.id, password: this.state.password, firstName: this.state.firstName, lastName: this.state.lastName, employeeId: this.state.employeeId, contactNumber: this.state.contactNumber};
-        UserService.editUser(user)
-            .then(res => {
-                this.setState({message : 'User added successfully.'});
-                this.props.history.push('/list-user');
-            });
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Container>
+          <Typography variant="h4" style={style}>
+            Izmjena korisnika
+          </Typography>
+          <form>
+            <TextField
+              label="USERNAME"
+              fullWidth
+              margin="normal"
+              name="username"
+              value={this.state.username}
+              disabled
+            />
 
-    render() {
-        return (
-            <React.Fragment>
-                <NavBar/>
-                <Container>
-                    <Typography variant="h4" style={style}>Izmjena korisnika</Typography>
-                    <form>
+            <TextField
+              label="IME"
+              fullWidth
+              margin="normal"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.onChange}
+            />
 
-                        <TextField label="USERNAME" fullWidth margin="normal" name="username" value={this.state.username} disabled/>
+            <TextField
+              label="PASSWORD"
+              type="password"
+              fullWidth
+              margin="normal"
+              name="password"
+              value={this.state.password}
+              onChange={this.onChange}
+            />
 
-                        <TextField label="IME" fullWidth margin="normal" name="firstName" value={this.state.firstName} onChange={this.onChange}/>
+            <TextField
+              label="PREZIME"
+              fullWidth
+              margin="normal"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.onChange}
+            />
 
-                        <TextField label="PASSWORD" type="password" fullWidth margin="normal" name="password" value={this.state.password} onChange={this.onChange}/>
+            <TextField
+              label="ZASTUPNIK ID"
+              fullWidth
+              margin="normal"
+              name="employeeId"
+              value={this.state.employeeId}
+              onChange={this.onChange}
+            />
 
+            <TextField
+              label="BROJ TELEFONA"
+              fullWidth
+              margin="normal"
+              name="contactNumber"
+              value={this.state.contactNumber}
+              onChange={this.onChange}
+            />
 
-                        <TextField label="PREZIME" fullWidth margin="normal" name="lastName" value={this.state.lastName} onChange={this.onChange}/>
+            <FormControl fullWidth style={styles.state}>
+              <InputLabel id="demo-simple-select-label">Entitet</InputLabel>
+              <Select
+                name="state"
+                value={this.state.state}
+                onChange={this.onChange}
+              >
+                <MenuItem value={1}>Republika Srpska</MenuItem>
+                <MenuItem value={2}>Federacija BiH</MenuItem>
+              </Select>
+            </FormControl>
 
-                        <TextField label="ZASTUPNIK ID" fullWidth margin="normal" name="employeeId" value={this.state.employeeId} onChange={this.onChange}/>
-
-                        <TextField label="BROJ TELEFONA" fullWidth margin="normal" name="contactNumber" value={this.state.contactNumber} onChange={this.onChange}/>
-
-                        <Button variant="contained" color="primary" onClick={this.saveUser}>Save</Button>
-                        <Button variant="contained" color="primary" onClick={() => this.props.history.push('/list-user')}>Odustani</Button>
-
-                    </form>
-                </Container>
-            </React.Fragment>
-        );
-    }
+            <Button variant="contained" color="primary" onClick={this.saveUser}>
+              Save
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.props.history.push("/list-user")}
+            >
+              Odustani
+            </Button>
+          </form>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
-const style ={
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: 50
-}
+const style = {
+  display: "flex",
+  justifyContent: "center",
+  marginTop: 50,
+};
+
+const styles = {
+  state: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+};
 
 export default EditUserComponent;
